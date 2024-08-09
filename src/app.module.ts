b1f5ec660a18188916f7df25453f7de805e96d92
ndkag <duykhang02vnn@gmail.com>
2024-08-09 10:58:06 +0700

    update mongooDB

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forRoot(
+      'mongodb+srv://khang123hy:khang123hy@cluster0.bj2kj.mongodb.net/213',
+    ),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
