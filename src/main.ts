a38bb19adfa7464dc4671e8138eb063aa881fdc3
ndkag <duykhang02vnn@gmail.com>
2024-08-09 14:36:56 +0700

    apply to validation

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,16 @@ import { AppModule } from './app.module';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { ConfigService } from '@nestjs/config';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
   const configService = app.get(ConfigService);
+  app.useGlobalPipes(new ValidationPipe());
 
-  app.useStaticAssets(join(__dirname, '..', 'public')); //truy cập được js, css ,images
-  app.setBaseViewsDir(join(__dirname, '..', 'views')); // truy cập views
-  app.setViewEngine('ejs');
+  // app.useStaticAssets(join(__dirname, '..', 'public')); //truy cập được js, css ,images
+  // app.setBaseViewsDir(join(__dirname, '..', 'views')); // truy cập views
+  // app.setViewEngine('ejs');
 
   await app.listen(configService.get<string>('PORT'));
 }
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,6 +1,13 @@
+import { IsEmail, IsNotEmpty } from 'class-validator';
+
 export class CreateUserDto {
+  @IsNotEmpty({ message: 'Email không được để trống' })
+  @IsEmail({}, { message: 'Email không hợp lệ a@a.a' })
   email: string;
+
+  @IsNotEmpty({ message: 'Password không được để trống' })
   password: string;
+
   name: string;
   phone: number;
   address: string;
