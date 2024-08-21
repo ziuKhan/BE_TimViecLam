bb98449a4f662a7f80fc336686c141d639248a68
ndkag <duykhang02vnn@gmail.com>
2024-08-21 08:36:05 +0700

    update health check

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,6 +17,7 @@ import { SubscribersModule } from './subscribers/subscribers.module';
 import { MailModule } from './mail/mail.module';
 import { ScheduleModule } from '@nestjs/schedule';
 import { ThrottlerModule } from '@nestjs/throttler';
+import { HealthModule } from './health/health.module';
 
 @Module({
   imports: [
@@ -51,6 +52,8 @@ import { ThrottlerModule } from '@nestjs/throttler';
         limit: 2,
       },
     ]),
+
+    HealthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
