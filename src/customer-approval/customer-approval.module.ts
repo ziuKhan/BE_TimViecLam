b467369fc8cfcf89d180ae5ff75bc9e70b90cbbb
Napoleon <duykhang02vnn@gmail.com>
2025-07-06 15:57:04 +0700

    up

--- src/customer-approval/customer-approval.module.ts
+++ src/customer-approval/customer-approval.module.ts
@@ -1,16 +1,17 @@
 import { Module } from '@nestjs/common';
-import { CustomerApprovalService } from './customer-approval.service';
-import { CustomerApprovalController } from './customer-approval.controller';
-import { MongooseModule } from '@nestjs/mongoose';
-import { CustomerApproval, CustomerApprovalSchema } from './schemas/customer-approval.schema';
-import { User } from '../decorator/customize';
-import { UserSchema } from '../users/schemas/user.schema';
-import { UsersModule } from '../users/users.module';
-import { Role, RoleSchema } from '../roles/Schemas/role.schema';
 import { ConfigModule } from '@nestjs/config';
-import { MailModule } from '../mail/mail.module';
-import { Company, CompanySchema } from '../companies/schemas/company.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CompaniesModule } from '../companies/companies.module';
+import { Company, CompanySchema } from '../companies/schemas/company.schema';
+import { MailModule } from '../mail/mail.module';
+import { Role, RoleSchema } from '../roles/Schemas/role.schema';
+import { UsersModule } from '../users/users.module';
+import { CustomerApprovalController } from './customer-approval.controller';
+import { CustomerApprovalService } from './customer-approval.service';
+import {
+  CustomerApproval,
+  CustomerApprovalSchema,
+} from './schemas/customer-approval.schema';
 
 @Module({
   imports: [
